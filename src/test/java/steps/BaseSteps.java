2898cc6ca4a4f11186c7444a6acf052523f3d4e6
aleks <aleksandar.stojanovic.91bgd@gmail.com>
2022-08-27 09:32:50 +0200

    added structure for basic cucumber and testng tests

---
+++ src/test/java/steps/BaseSteps.java
@@ -0,0 +1,24 @@
+package steps;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import tests.BaseTest;
+
+public class BaseSteps extends BaseTest {
+
+    @Before
+    public void setup() throws Exception {
+        init("CHROME",30);
+    }
+
+    @After
+    public void tearDown(){
+        quit();
+    }
+
+    @Given("I navigate to google.com")
+    public void iNavigateToGoogleCom() {
+        driver.get("http://www.google.com");
+    }
+}
