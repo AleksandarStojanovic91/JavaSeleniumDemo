0af19ad8c8fd7d4d24cbd965726872cda1c00cb0
aleks <aleksandar.stojanovic.91bgd@gmail.com>
2022-08-27 09:35:46 +0200

    added basic test data extraction from excel

--- src/test/java/steps/BaseSteps.java
+++ src/test/java/steps/BaseSteps.java
@@ -1,11 +1,17 @@
 package steps;
 
+import excel.ExcelUtilities;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import tests.BaseTest;
 
+import java.io.IOException;
+import java.util.Map;
+
 public class BaseSteps extends BaseTest {
+
+    Map<String, String> data;
 
     @Before
     public void setup() throws Exception {
@@ -20,5 +26,10 @@ public class BaseSteps extends BaseTest {
     @Given("I navigate to google.com")
     public void iNavigateToGoogleCom() {
         driver.get("http://www.google.com");
+    }
+
+    @Given("I have test data in {string} {string} {string}")
+    public void iHaveTestDataIn(String file, String sheet, String row) throws IOException {
+        data = new ExcelUtilities().getRowData(file,sheet,Integer.parseInt(row));
     }
 }
